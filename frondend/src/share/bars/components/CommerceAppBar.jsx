7d7b1925126138388e4daeeb8bcbb07ca33465fe
KennRobot <kebrgonzalezdo@ittepic.edu.mx>
2024-11-20 15:51:26 -0800

    con logos

--- frondend/src/share/bars/components/CommerceAppBar.jsx
+++ frondend/src/share/bars/components/CommerceAppBar.jsx
@@ -82,24 +82,21 @@ function ResponsiveAppBar() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
+          <Box
             component="a"
             href="/"
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
               textDecoration: 'none',
             }}
           >
-            LOGO
-          </Typography>
+            <img
+              src="/public/files/images/logoteam4.png"
+              alt="Logo"
+              style={{ height: '40px', width: 'auto' }}
+            />
+          </Box>
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
