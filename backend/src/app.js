081fff1552434de95071878d66337e3734727efa
dev-aj001 <jairibaparr@outlook.com>
2024-11-19 22:40:14 -0700

    create one inventory

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const inventoryRoutes = require('./routes/inventoryRoutes');
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/api/v1/inventory', inventoryRoutes);
+
+app.get('/', (req, res, next) => {
+    res.send(
+        `<h1>API RESTFULL de Inventarios</p>`
+    );
+});
+
+app.use((req, res, next) => {
+    res.status(404).json({ code: 404, message: 'Ruta no encontrada' });
+});
+
+module.exports = app;
