6958bb37b14762100915e40fb9a28993d52dedd2
Cesar Hernandez <149346749+cesarurielhr@users.noreply.github.com>
2024-11-19 22:45:00 -0700

    Get por id y all compleatado

--- backend/src/controllers/inventoryController.js
+++ backend/src/controllers/inventoryController.js
@@ -1,5 +1,27 @@
 const inventaryModel = require("../models/inventaryModel");
 
+async function getallInvetory(req, res) {
+  try {
+    const inventary = await inventaryModel.find();
+    res.status(200).json(inventary);
+  } catch (error) {
+    res.status(500).json({ message: "Error al obtener la lista de negocios", error: error.message });
+  }
+}
+
+async function getOneInvetory(req, res) {
+  const idNegocio = req.params.id;
+  try {
+    const negocio = await inventaryModel.findById(idNegocio);
+    if (!negocio) {
+      return res.status(404).json({ message: "Negocio no encontrada " });
+    }
+
+    res.status(200).json(negocio);
+    } catch (error) {
+    res.status(500).json({ message: "Error al obtener a un negocios", error: error.message });
+  }
+}
 async function deleteOneInvetory(req, res) {
   const IdNegocio = req.params.id;
  console.log(IdNegocio);
@@ -18,5 +40,7 @@ async function deleteOneInvetory(req, res) {
 }
 
 module.exports = {
+  getallInvetory,
+  getOneInvetory,
   deleteOneInvetory,
 }
--- backend/src/routes/inventoryRoutes.js
+++ backend/src/routes/inventoryRoutes.js
@@ -6,7 +6,8 @@ const inventoryController = require('../controllers/inventoryController');
 
 const router = express.Router();
 
-
+router.get('/',inventoryController.getallInvetory);
+router.get('/negocio/:id', inventoryController.getOneInvetory);
 router.delete('/inventory/:id',inventoryController.deleteOneInvetory);
 
 module.exports = router;
