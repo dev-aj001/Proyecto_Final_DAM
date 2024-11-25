17ca36f3a1b41ee84f05694954c360bdff891711
dev-aj001 <jairibaparr@outlook.com>
2024-11-25 12:48:40 -0700

    ruta delete corregida

--- backend/src/routes/almacenesRoutes.js
+++ backend/src/routes/almacenesRoutes.js
@@ -16,7 +16,7 @@ router.get('/:id/almacen', almacenController.readAllbyId);
 router.get('/:id/almacen/:id_almacen', almacenController.readOne);
 router.get('/almacenes', almacenController.readAll);
 router.put('/:id/almacen/:id_almacen', almacenController.updateOne);
-router.delete('/:id/almacen/:id_almacen', almacenController.updateOne);
+router.delete('/:id/almacen/:id_almacen', almacenController.deleteOne);
 
 
 
